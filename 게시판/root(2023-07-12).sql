b2b05c419d7812105cf342cbc881362ebd15c1f1
callor <callor@callor.com>
2023-07-14 11:47:17 +0900

    게시판_이미지

--- 게시판/root(2023-07-12).sql
+++ 게시판/root(2023-07-12).sql
@@ -18,9 +18,47 @@ CREATE TABLE tbl_bbs (
 	b_content	text,			
 	b_count	int,			
 	b_update	datetime			
-
 );
 
+CREATE TABLE tbl_user (
+	username	varchar(125) PRIMARY KEY,
+	nickname	varchar(125),		
+	tel	varchar(20)		
+);
+SELECT COUNT(*) FROM tbl_bbs;
+
+INSERT INTO tbl_user(username, nickname,tel)
+VALUES('callor@callor.com','내멋으로','010-1111-1111');
+COMMIT ;
+
+-- EQ JOIN 
+-- 두 Table 이 완전한 참조무결성관계에 있을때만
+-- 정상적인 데이터가 나타난다
+-- tbl_bbs 데이터에 b_username 칼럼의 데이터가
+-- callor@callor.com 으로 되어 있는데
+-- 이 데이터가 tbl_user 테이블에 있으면 정상적으로
+-- 출력이되고, 그렇지 않으면 데이터가 누락되는 현상
+SELECT B.b_seq, B.b_pseq, B.b_date,
+	B.b_time, B.b_username, U.nickname,
+	B.b_subject, B.b_content, B.b_count, B.b_update  
+FROM tbl_bbs B, tbl_user U
+WHERE B.b_username = U.username
+		LIMIT 10;
+        
+-- tbl_bbs 테이블과 tbl_user 테이블이 완전 참조 무결성
+-- 관계인지 검증하는 SQL
+-- LEFT JOIN 은 참조무결성과 관계없이 JOIN 결과를
+-- 정상적으로 확인 가능하다
+-- 이때 N:1 의 관계에서 NULL 인 데이터가 있으면
+-- 테이블간에 참조무결성관계가 성립되지 않는다
+-- 이 테이블간에는 FK 설정이 불가능하다
+SELECT * 
+FROM tbl_bbs B
+	LEFT JOIN tbl_user U
+		ON B.b_username = U.username
+WHERE U.nickname IS NULL;        
+
+DELETE FROM tbl_user WHERE username = 'callor@callor.com';
 
 
 
---
+++ 게시판/root(2023-07-14).sql
@@ -0,0 +1,45 @@
+-- root 로 접속한 화면
+USE bbsDB;
+
+DESC tbl_bbs;
+-- tbl_bbs 테이블에 대표이미지를 저장하는 칼럼 추가
+-- tbl_bbs 테이블에 b_image 라는 칼럼을 추가하는데
+-- 데이터 타입은 VARCHAR(125) 로 하고
+-- b_count 다음에 (b_update) 앞에 위치하도록
+-- ALTER TABLE 사용하여 새로운 칼럼을 추가하는 경우
+-- 제약사항(NOT NULL 등)을 동시에 추가할수 없다
+ALTER TABLE tbl_bbs
+ADD b_image VARCHAR(125)
+AFTER b_count;
+DESC tbl_bbs;
+
+-- Table 을 DROP 하고 
+-- 다시 같은 구조로 CREATE TABLE
+TRUNCATE tbl_bbs;
+
+-- FK 삭제
+ALTER TABLE tbl_bbs
+DROP CONSTRAINT  FK이름;
+
+-- 닉네임 칼럼 삭제
+ALTER TABLE tbl_bbs
+DROP b_nickname;
+
+
+
+
+INSERT INTO tbl_bbs(
+	b_pseq,		b_date,		b_time,		b_username,
+	b_subject,	b_content,	b_count,	b_image
+) VALUES (
+	1,1,1,1,1,1,1,1
+);    
+SELECT * FROM tbl_bbs;
+
+CREATE TABLE tbl_images (
+	i_seq	bigint	PRIMARY KEY	AUTO_INCREMENT,
+	i_bseq	bigint	NOT NULL,
+	i_originalName	VARCHAR(125),			
+	i_uploadName	VARCHAR(255)			
+);
+
