f1efd9ce3ea4f2543e8c00d2e93855cc2d2abe41
callor <callor@callor.com>
2023-07-17 10:51:00 +0900

    bbs

--- 게시판/root(2023-07-14).sql
+++ 게시판/root(2023-07-14).sql
@@ -42,4 +42,15 @@ CREATE TABLE tbl_images (
 	i_originalName	VARCHAR(125),			
 	i_uploadName	VARCHAR(255)			
 );
+DESC tbl_bbs;
+SELECT * FROM tbl_bbs;
+
+INSERT INTO tbl_bbs (b_username)
+VALUES('callor');
+-- AUTO_INCREMENT 로 설정된 칼럼의 가장 
+-- 마지막 INSERT 된 데이터 가져오기
+SELECT LAST_INSERT_ID();
+
+
+
 
